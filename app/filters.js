287b35950c0a0cffa41b651dbaaf78d11bfea5e0
Maulik Jani <maulik.jani1@engineering.digital.dwp.gov.uk>
2023-12-15 14:14:11 +0000

    Update filters.js

--- app/filters.js
+++ app/filters.js
@@ -32,3 +32,27 @@ const addFilter = govukPrototypeKit.views.addFilter
 
 // Add the filters using the addFilter function
 Object.entries(filters).forEach(([name, fn]) => addFilter(name, fn))
+
+
+
+
+
++ const prototypeFilters = require('@x-govuk/govuk-prototype-filters');
+
+  module.exports = function (env) {
+    /**
+     * Instantiate object used to store the methods registered as a
+     * 'filter' (of the same name) within nunjucks. You can override
+     * gov.uk core filters by creating filter methods of the same name.
+     * @type {Object}
+     */
+-   var filters = {}
++   var filters = prototypeFilters
+
+    // Existing filter
+    filters.sayHi = function(name) {
+        return 'Hi ' + name + '!'
+    }
+
+    return filters
+  }
