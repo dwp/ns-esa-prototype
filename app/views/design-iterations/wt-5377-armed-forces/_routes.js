71507053c0f36efd0a5fa23bac1b38f75b017050
Becky Lee <becky.lee@engineering.digital.dwp.gov.uk>
2025-07-28 13:28:27 +0000

    Initial commit
    GitOrigin-RevId: 9b3c83081d704b0b9e53ed08b6939f562b69a36d

--- app/views/design-iterations/wt-5377-armed-forces/_routes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const BASE_PATH = 'design-iterations/wt-5377-armed-forces';
-const ABS_BASE_PATH = `/${BASE_PATH}`;
-const NEXT_PATH = '/design-iterations/wt-5377-armed-forces';
-
-
-module.exports = router;
